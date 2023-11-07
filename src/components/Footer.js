dd6ae7efb679415cc0b9eb2065aaab5d4201c0a9
shubhankars123 <satpathyshubhankar@gmail.com>
2023-11-07 20:16:27 +0530

    Initial Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://github.com/shubhankars123"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        github.com/shubhankars123
+                    </a>
+
+                    <p>
+                        Built by | &nbsp;
+                       <b>
+                            Shubhankar Satpathy
+                       </b>
+                        
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
